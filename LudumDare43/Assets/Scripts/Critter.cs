334da0cb862a5a881bc16acdcdf19818b23efa5a
MCjammydodger <jimmer.hook@btinternet.com>
2018-12-01 11:32:39 +0000

    Added a Critter

---
+++ LudumDare43/Assets/Scripts/Critter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Critter : MonoBehaviour {
+
+    private Player player;
+    private NavMeshAgent navAgent;
+
+    private void Awake()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        player = FindObjectOfType<Player>();
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        navAgent.SetDestination(player.transform.position);
+	}
+}
