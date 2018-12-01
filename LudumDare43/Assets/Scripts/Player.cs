be41a9c7426a69e71fb0b99d6eb70381d68a9a99
MCjammydodger <jimmer.hook@btinternet.com>
2018-12-01 11:03:16 +0000

    Added the ability to jump.

--- LudumDare43/Assets/Scripts/Player.cs
+++ LudumDare43/Assets/Scripts/Player.cs
@@ -6,9 +6,13 @@ public class Player : MonoBehaviour {
 
     [SerializeField]
     private float walkSpeed = 2.0f;
+    [SerializeField]
+    private float jumpSpeed = 2.0f;
 
     private CharacterController cc;
     private Camera mainCamera;
+
+    private Vector3 movementVector;
 
     private void Awake()
     {
@@ -18,27 +22,45 @@ public class Player : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
+        movementVector = new Vector3();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 movementVector = new Vector3();
-        movementVector.x = Input.GetAxis("Horizontal");
-        movementVector.z = Input.GetAxis("Vertical");
+        Vector3 inputVector = new Vector3();
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.z = Input.GetAxis("Vertical");
 
         // Move relative to camera direction.
-        movementVector = mainCamera.transform.TransformDirection(movementVector);
+        inputVector = mainCamera.transform.TransformDirection(inputVector);
 
         // Keep walking grounded.
-        movementVector.y = 0;
+        inputVector.y = 0;
 
         // Normalise the vector to ensure speed is equal in all directions.
-        movementVector.Normalize();
-
-        cc.Move(movementVector * walkSpeed * Time.deltaTime);
+        inputVector.Normalize();
 
         // Rotate player to face the direction they are moving.
+        RotatePlayer(inputVector);
+
+        if(Input.GetButtonDown("Jump") && cc.isGrounded)
+        {
+            movementVector.y = jumpSpeed;
+        }
+        // Add gravity to the player's movement.
+        if (!cc.isGrounded)
+        {
+            movementVector += Physics.gravity * Time.deltaTime;
+        }
+
+        movementVector.x = inputVector.x;
+        movementVector.z = inputVector.z;
+
+        cc.Move(movementVector * walkSpeed * Time.deltaTime);
+    }
+
+    private void RotatePlayer(Vector3 movementVector)
+    {
         if (movementVector != Vector3.zero)
         {
             Quaternion lookRotation = new Quaternion();
