2100e628b6160f1d38a1bfeb84eb0c510dbbd394
MCjammydodger <jimmer.hook@btinternet.com>
2018-12-01 10:49:40 +0000

    Make camera follow the player.

---
+++ LudumDare43/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.position + offset;
+	}
+}
