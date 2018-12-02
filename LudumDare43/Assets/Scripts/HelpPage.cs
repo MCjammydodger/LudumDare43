bc6060e069a6f10783800926b62f8d40ace06642
MCjammydodger <jimmer.hook@btinternet.com>
2018-12-02 16:23:19 +0000

    Created help page.

---
+++ LudumDare43/Assets/Scripts/HelpPage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpPage : MonoBehaviour {
+
+    [SerializeField]
+    private Transform listParent;
+
+    [SerializeField]
+    private UnityEngine.UI.Button buttonPrefab;
+
+    [SerializeField]
+    private HelpPanel helpPanel;
+
+    private void Start()
+    {
+        foreach(HelpPanel.Help help in helpPanel.helpList)
+        {
+            UnityEngine.UI.Button newButton = Instantiate(buttonPrefab, listParent);
+            Text text = newButton.GetComponentInChildren<Text>();
+            text.text = help.title;
+            newButton.onClick.AddListener(() => { helpPanel.SetupHelpPanel(help.type, () => gameObject.SetActive(true)); gameObject.SetActive(false); }); 
+        }
+    }
+}
---
+++ LudumDare43/Assets/Scripts/HelpPanel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class HelpPanel : MonoBehaviour {
+
+    [SerializeField]
+    private RawImage image;
+    [SerializeField]
+    private Text title;
+    [SerializeField]
+    private Text info;
+
+    public enum HelpType { GOAL, CRITTERS, CAGE, PICK_UP, LEVERS, BUTTONS, SACRIFICE, WATER, TRAPS}
+
+    [Serializable]
+    public class Help
+    {
+        public HelpType type;
+        public Texture image;
+        public string title;
+        [TextArea]
+        public string info;
+    }
+
+    public List<Help> helpList;
+
+    private Action callback;
+
+	public void SetupHelpPanel(HelpType helpType, Action clbk)
+    {
+        gameObject.SetActive(true);
+        callback = clbk;
+        foreach(var help in helpList) {
+            if (help.type == helpType)
+            {
+                if (help.image != null)
+                {
+                    image.gameObject.SetActive(true);
+                    image.texture = help.image;
+                }
+                else
+                {
+                    image.gameObject.SetActive(false);
+                }
+                title.text = help.title;
+                info.text = help.info;
+                return;
+            }
+        }
+        Debug.LogError("Haven't set up help for: " + helpType);
+        ClosePanel();     
+    }
+
+    public void ClosePanel()
+    {
+        if (callback != null)
+        {
+            callback();
+        }
+        gameObject.SetActive(false);
+    }
+}
